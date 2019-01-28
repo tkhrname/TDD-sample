a8635918653d43d1655e1739d2739a078cebff70
渡邊丈洋 <takehiro.watanabe.224@gmail.com>
2019-01-28 23:57:06 +0900

    Initial Commit

---
+++ MyAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlert
+//
+//  Created by 渡邊丈洋 on 2019/01/28.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
